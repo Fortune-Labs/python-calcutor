8b7aef88e95293a0be54df38dd2cd27e6b2dce79
Sabaly99 <naniasabaly99@gmail.com>
2026-02-10 23:20:07 +0000

    New code added to make automation by user easier

--- main.py
+++ main.py
@@ -9,4 +9,20 @@ def subtrac(a,b):
     return a-b
 
 def divide(a,b):
+    if b == 0
+        print("Error : divison by zero error!!")
+
     return a/b
+
+def multiply(a,b):
+    return a*b
+
+
+
+while true:
+
+    print("Option 1 : ADDITON")
+    print("Option 2 : SUBTRACTION")
+    print("Option 1 : DIVISION")
+    print("Option 1 : MULTIPLICATION")
+    print("Option 1 : EXIT")
